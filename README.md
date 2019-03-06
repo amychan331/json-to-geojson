ed87d1c7f5e35680a8fe409da179899ee8577683
amychan331@yahoo.com <amychan331@yahoo.com>
2019-03-05 18:40:43 -0800

    initial push

---
+++ README.md
@@ -0,0 +1,10 @@
+# JSON to GEOJSON
+
+## Description
+
+## Technology
+Python
+
+## Screenshot
+
+<kbd>![Corrected names](https://github.com/amychan331/devcaps/blob/master/assets/screen-shot01.png)</kbd>
---
+++ json_to_geojson.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+from sys import exit
+from json import load, dump
+
+def read_file():
+    try:
+        inputfile = input("Filename: ").strip(' ')
+        if inputfile[-5:] != '.json':
+            raise ValueError('Error: Input file is not JSON file')
+
+        proppath = input("If the object holding the properties data in the JSON file is nested, input the path with comma separated each key (example: data,stations,0). If not, just press enter: ").strip(' ').split(",")
+
+        propkey = input("Input the property keys in the JSON file that you want inside the geojson's properties object, with comma separated each key (example: name, age, height) ").strip(' ').split(",")
+        if not propkey: raise ValueError('Error: Property keys are required.')
+
+        longkey = input("JSON file's property key for longitude: ").strip(' ')
+        if not longkey: raise ValueError('Error: Empty longitude input')
+        latkey = input("JSON file's property key for latitude: ")
+        if not latkey: raise ValueError('Error: Empty latitude input').strip(' ')
+
+        fp = open(inputfile, 'r')
+        jsonfile = load(fp)
+        fp.close()
+
+        propobj = get_properties_obj(jsonfile, proppath, propkey)
+        return propobj, propkey, longkey, latkey
+
+    except IOError:
+        print('Could not read file: ', inputfile)
+        exit()
+
+    except Exception as e:
+        print(e)
+        exit()
+
+
+def get_properties_obj(file, path):
+    if path and path[0] != '':
+        for p in path:
+            if p.isdigit():
+                p = int(p)
+            file = file[p]
+        return file
+
+    return file
+
+
+def create_geojson(propobj, propkey, longkey, latkey):
+    geojson = {}
+    geojson['type'] = 'FeatureCollection'
+
+    features = []
+    for prop in propobj:
+        feature = {}
+        feature['type'] = 'Feature'
+        feature['geometry'] = {
+            'type':'Point',
+            'coordinates':[ prop[longkey], prop[latkey] ]
+        }
+
+        properties = {}
+        for key in propkey:
+            properties[key] = prop[key]
+
+        feature['properties'] = properties
+        features.append(feature)
+
+    geojson['features'] = features
+    return geojson
+
+
+def main():
+    propobj, propkey, longkey, latkey = read_file()
+    geojson = create_geojson(properties, propkey, longkey, latkey)
+
+    with open('convert.geojson', 'w') as f:
+        dump(geojson, f, indent=2)
+    f.close()
+
+
+if __name__ == '__main__':
+    main()
